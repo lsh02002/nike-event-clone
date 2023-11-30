8b88035381bb28ee3b8d6c5024d4515e090fdc24
lsh02002 <40713089+lsh02002@users.noreply.github.com>
2023-11-30 17:49:59 +0900

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import EventPage from "./components/EventPage";
+import GiftPage from "./components/GiftPage";
 
 function App() {
   return (
     <div className="App">
       <EventPage />
+      <GiftPage />
     </div>
   );
 }
